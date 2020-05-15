8cd21aa775244ea903c9b813e510f5e0f75fbf82
Zheyuan Zhang <56742253+JasonZhangzy1757@users.noreply.github.com>
2020-05-14 23:57:49 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -88,9 +88,9 @@ class BaseTrainer(object):
         for i, batch in enumerate(data_loader, start=1):
             input_ids, input_mask, seg_ids, start_positions, end_positions, _ = batch
             
-            seq_len = torch.sum(torch.sign(input_ids), 1).detach()
-            max_len = torch.max(seq_len).detach()
-          
+            seq_len = torch.sum(torch.sign(input_ids), 1).detach()
+            max_len = torch.max(seq_len).detach()
+            
             
            #if self.args.use_cuda:
            #    input_ids = input_ids.cuda(self.args.gpu, non_blocking=True)
